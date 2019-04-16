dc2a54fb262c46d9d8fdd93647b81399d0bb9104
Leonie Dong <leoniedong@hotmail.com>
2019-04-16 20:53:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def dictionary
+  dictionary = {:hello => "hi",
+  :to => "2",
+  :two => "2",
+  :too => "2",
+  :for => "4",
+  :four => "4",
+  :you => "u",
+  :at => "@",
+  :and => "&"
+  }
+end
+
+def word_substituter(string)
+  array = string.split(" ")
+  array.map do |word|
+    dictionary.map do |key, value|
+      if word == key
+        word = value
+      end
+    end
+    end
+    array.join(" ")
+end
